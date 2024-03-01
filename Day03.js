29017368cda0f566dc026fcd1cadd557c3775e5a
Moteeullah Azmi <you@example.com>
2024-03-01 18:05:47 +0530

    azmi

---
+++ Day03.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    if(x===val){
+       console.log("value:",toBoolean);
+    }
+    else if(y!==val){
+        console.log("Not Equal");
+    } 
+   return  [
+    tobe(val)
+    ,notToBE(val)
+]
+}
+
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
